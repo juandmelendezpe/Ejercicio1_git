e0c3c5bf989a137f4e7c54e8980f7ae7eb971478
Juan MP <118199373+juandmelendezpe@users.noreply.github.com>
2024-01-30 09:40:18 +0100

    update files
    update files

---
+++ ejercicio1/src/main/java/Trabajo_ED_Git/ejercicio1/App.java
@@ -0,0 +1,16 @@
+package Trabajo_ED_Git.ejercicio1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Hola Juan");
+        System.out.println("Hola Yari");
+
+    }
+}
